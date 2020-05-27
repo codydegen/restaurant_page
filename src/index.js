a2bba3253040e06f4a761cb4a606196697e5698b
codydegen <codydegen@gmail.com>
2020-05-27 10:14:00 -0400

    Create static mockup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+import './style.css';
+import Icon from './icon.png';
+// +   // Add the image to our existing div.
+// +   const myIcon = new Image();
+// +   myIcon.src = Icon;
+// +
+// +   element.appendChild(myIcon);
+
+
 console.log("hello2")
 
 function testFunction() {
